a4e6996dc6a4239b1bbca33de40f8555e27a7045
juiceqa <gglick@juicepharma.com>
2021-10-07 11:14:36 -0400

    :skull: NOT READY FOR USAGE :skull:

--- cypress/page-objects/litmus/LitmusBuilderPage.js
+++ cypress/page-objects/litmus/LitmusBuilderPage.js
@@ -1,72 +1,74 @@
 class LitmusBuilderPage {
+  open() {
+    cy.visit("/folders/78809/emails/2929982/builder")
+    this.waitForPageLoading()
+  }
 
-    open() {
-        cy.visit('/folders/unsorted_emails/emails/2650141/builder');
-        this.waitForPageLoading();
-    }
+  waitForPageLoading() {
+    cy.wait("@category")
+  }
 
-    waitForPageLoading() {
-        cy.wait('@category');
-    }
+  clickRunEmailPreviews() {
+    cy.get("iframe.builder").then(function ($iframe) {
+      const $body = $iframe.contents().find("body")
+      cy.wrap($body).contains("Email Previews").click({ force: true })
+    })
+  }
 
-    clickRunEmailPreviews() {
-        cy.get('iframe.builder').then(function ($iframe) {
-            const $body = $iframe.contents().find('body');
-            cy.wrap($body).contains("Email Previews").click({force: true});
+  selectConfiguration(deviceName) {
+    cy.wait(Cypress.env("waitTime"))
+    cy.get("iframe.builder").then(function ($iframe) {
+      const $body = $iframe.contents().find("body")
+      cy.wrap($body).contains(deviceName).should("exist").click({ force: true })
+    })
+  }
+
+  closeConfigView() {
+    cy.get("iframe.builder").then(function ($iframe) {
+      const $body = $iframe.contents().find("body")
+      cy.wrap($body).find(".icon-cross").first().click({ force: true })
+    })
+  }
+
+  downloadImage(fullPath, fileName) {
+    cy.wait(Cypress.env("waitTime"))
+    cy.get("iframe.builder").then(function ($iframe) {
+      const $body = $iframe.contents().find("body")
+      cy.wrap($body)
+        .find(".result img")
+        .then(($image) => {
+          cy.wrap($image).then(() => {
+            const imageUrl = $image.first().attr("src")
+            cy.log("imageUrl: " + imageUrl)
+            cy.downloadFile(imageUrl, fullPath, fileName)
+          })
         })
-    }
+    })
+  }
 
-    selectConfiguration(deviceName) {
-        cy.wait(Cypress.env('waitTime'));
-        cy.get('iframe.builder').then(function ($iframe) {
-            const $body = $iframe.contents().find('body');
-            cy.wrap($body).contains(deviceName).should('exist').click({force: true});
-        })
-    }
+  saveNotificationForConfig(config, name_part) {
+    const fullPath = `cypress/fixtures/${Cypress.env(
+      "notificationScreenFolder"
+    )}`
+    const actualFileName = `${config} ${name_part}.png`
+    this.downloadImage(fullPath, actualFileName)
+  }
 
+  getFixtureFromFolder(folderName, fileName) {
+    return cy.fixture(`${folderName}/${fileName}`)
+  }
 
-    closeConfigView() {
-        cy.get('iframe.builder').then(function ($iframe) {
-            const $body = $iframe.contents().find('body');
-            cy.wrap($body).find('.icon-cross').first().click({force: true});
-        })
-    }
-
-    downloadImage(fullPath, fileName) {
-        cy.wait(Cypress.env('waitTime'));
-        cy.get('iframe.builder').then(function ($iframe) {
-            const $body = $iframe.contents().find('body');
-            cy.wrap($body).find('.result img').then(($image => {
-                cy.wrap($image).then(() => {
-                    const imageUrl = $image.first().attr('src');
-                    cy.log("imageUrl: " + imageUrl);
-                    cy.downloadFile(imageUrl, fullPath, fileName);
-                })
-            }))
-        })
-    }
-
-    saveNotificationForConfig(config, name_part) {
-        const fullPath = `cypress/fixtures/${Cypress.env('notificationScreenFolder')}`;
-        const actualFileName = `${config} ${name_part}.png`;
-        this.downloadImage(fullPath, actualFileName);
-    }
-
-    getFixtureFromFolder(folderName, fileName) {
-        return cy.fixture(`${folderName}/${fileName}`)
-    }
-
-    routeRequests() {
-        cy.server()
-        cy.route({
-            method: 'GET',
-            url: '/checklist/usage/status',
-        }).as('status');
-        cy.route({
-            method: 'GET',
-            url: '/checklist/tests/*/cat',
-        }).as('category');
-    }
+  routeRequests() {
+    cy.server()
+    cy.route({
+      method: "GET",
+      url: "/checklist/usage/status",
+    }).as("status")
+    cy.route({
+      method: "GET",
+      url: "/checklist/tests/*/cat",
+    }).as("category")
+  }
 }
 
-export default new LitmusBuilderPage();
+export default new LitmusBuilderPage()
--- cypress/page-objects/litmus/LitmusLoginPage.js
+++ cypress/page-objects/litmus/LitmusLoginPage.js
@@ -1,18 +1,17 @@
-
-import LitmusBuilderPage from "./LitmusBuilderPage";
+import LitmusBuilderPage from "./LitmusBuilderPage"
 
 class LitmusLoginPage {
   open() {
-    cy.viewport(1280, 720);
-    cy.visit('/sessions/new');
+    cy.viewport(1280, 720)
+    cy.visit("/folders/78809/emails/3555576/checklist")
   }
 
   login(login, password) {
-    cy.get('#email').type(login);
-    cy.get('#password').type(password);
-    cy.get('button:contains("Sign in")').click({force: true});
-    cy.wait('@status');
+    cy.get("#email").type(login)
+    cy.get("#password").type(password)
+    cy.get('button:contains("Sign in")').click({ force: true })
+    cy.wait("@status")
   }
 }
 
-export default new LitmusLoginPage();
+export default new LitmusLoginPage()
